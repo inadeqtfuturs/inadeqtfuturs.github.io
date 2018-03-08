b21de75b6de4e8ed6d5d574eee67fd43b38d79da
inadeqtfuturs <AlexJ.Christie@gmail.com>
2018-03-08 11:38:45 -0600

    20180308 - search and sw update build

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Set this to true for production
+var doCache = false;
+
+// Name our cache
+var CACHE_NAME = 'if-cache';
+
+// delete old caches that are not our current one.
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys()
+      .then(keyList =>
+        Promise.all(keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            console.log('Deleting cache: ' + key)
+            return caches.delete(key);
+          }
+        }))
+      )
+  );
+});
+
+var urlsToCache = [
+  '/',
+  '/css/style.css',
+  '/js/lunr.min.js'
+];
+
+// first time user starts website, 'install' is triggered
+self.addEventListener('install', function(event) {
+  if (doCache) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          return cache.addAll(urlsToCache);
+        })
+    );
+  }
+});
+
+self.addEventListener('fetch', function(event) {
+  if (doCache) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
