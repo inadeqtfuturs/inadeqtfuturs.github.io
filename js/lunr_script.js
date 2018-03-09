831161a090a640b075cf0a6c2bbf21ec13c7873f
inadeqtfuturs <AlexJ.Christie@gmail.com>
2018-03-09 07:46:06 -0600

    search update

--- js/lunr_script.js
+++ js/lunr_script.js
@@ -1,78 +1,90 @@
-var lunrIndex, $results, pagesIndex;
+/*
+  load search results page w/ lunr.
+*/
 
-function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split('&');
+var lunrIndex
 
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split('=');
-
-    if (pair[0] === variable) {
-      return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-    }
-  }
-}
-
-var query = getQueryVariable('q');
-
-// Initialize lunrjs using our generated index file
-function initLunr() {
-    // First retrieve the index file
+// initialize lunrjs using generated index file.
+  function initLunr() {
+    // retrieve index file.
     $.getJSON("/index.json")
-        .done(function(index) {
-            pagesIndex = index;
-            console.log("index:", pagesIndex);
-            lunrIndex = lunr(function() {
-                this.field("title", { boost: 2 });
-                this.field("tags", { boost: 4 });
-                this.field("content", { boost: 10 });
-                this.ref("uri");
+      .done(function(index) {
+        pagesIndex = index;
+        // put that index file in console for safe keeping.
+        console.log("index", pagesIndex);
+        lunrIndex = lunr(function() {
+          this.field("title", { boost: 2 });
+          this.field("tags", { boost: 4 });
+          this.field("content", { boost: 10 });
+          this.ref("uri");
 
-                pagesIndex.forEach(function (page) {
-                    this.add(page)
-                }, this)
-            });
-        })
-        .fail(function(jqxhr, textStatus, error) {
-            var err = textStatus + ", " + error;
-            console.error("Error getting Hugo index flie:", err);
+          pagesIndex.forEach(function (page) {
+            this.add(page)
+          }, this)
         });
-}
-
-function search(query) {
-    return lunrIndex.search(query).map(function(result) {
-            return pagesIndex.filter(function(page) {
-                return page.uri === result.ref;
-            })[0];
-        });
-}
-
-function renderResults(results) {
-  if (!results.length) {
-    return;
+      })
+      .fail(function(jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error;
+        console.error("Error getting Hugo index file:", err);
+      });
   }
 
-  results.slice(0, 10).forEach(function(result) {
-    var $result = $("<li>");
-    $result.append($("<a>", {
+// initialize lunr.
+  initLunr();
+
+// search the lunrIndex.
+  function search(query) {
+    return lunrIndex.search(query).map(function(result) {
+      return pagesIndex.filter(function(page) {
+        return page.uri === result.ref;
+          })[0];
+        });
+  }
+
+// render the results in html.
+  function renderResults(results) {
+    if (!results.length) {
+      return;
+    }
+
+    results.slice(0, 10).forEach(function(result) {
+      var $result = $("<li>");
+      $result.append($("<a>", {
         href: result.uri,
         text: result.title
-    }));
-    $results.append($result);
-  });
-}
-
-
-function initUI() {
-  $results = $("#results");
-  if (query.length < 3) {
-    return;
+      }));
+      $results.append($result);
+    });
   }
-  renderResults(search(query));
-}
 
-// Let's get started
-initLunr();
+// putting it all together.
+
+  function initUI() {
+    // get the searchTerm from url
+      function getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+
+        for (var i = 0; i < vars.length; i++) {
+          var pair = vars[i].split('=');
+
+          if (pair[0] === variable) {
+            return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+          }
+        }
+      }
+
+    // register searchTerm.
+      var query = getQueryVariable('q');
+      $results = $("#results");
+      if (query.length < 3) {
+        return;
+      }
+
+      var results = search(query)
+      renderResults(results);
+  }
+
 $(document).ready(function() {
   initUI();
 });
